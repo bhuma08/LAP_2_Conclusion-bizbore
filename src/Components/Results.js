360647e4bb11f991cd7562f3931f4e28f4a98598
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-08-27 20:36:22 +0100

    tv show results in grid
    -scroll next
    -styling

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -9,19 +9,23 @@ class Results extends Component {
       
         return this.props.results.map((item, index) => (
             <div id="tvShowList" key={index}>
-                
-                <h3>Name: {item.show.name}</h3>
                 {/* <img src={item.show.image.medium} alt="hehe"/><br></br> */}
-                {item.show.image ? <img src={item.show.image.medium} alt="image of the show"/> : null}<br></br>
-                Genre: {item.show.genres.map((items, idx)=>
-                <span key={idx}>
-                    <span>{items} </span>
-                </span>)}
-                <p>Language: {item.show.language}</p>
-                <p>Status: {item.show.status}</p>
-                {item.show.summary ? <p id="summary">Summary: {item.show.summary.replace('<p>','').replace('</p>','').replace('<b>','').replace('</b>','').replace('<i>','').replace('</i>','')}</p> : null} 
-                <p>Ratings: {item.show.rating.average}</p>
-                
+
+                <div>{item.show.image ? <img src={item.show.image.medium} alt="image of the show"/> : null}</div>
+
+                <div>
+                    Name: {item.show.name}<br></br>
+               
+                    Genre: {item.show.genres.map((items, idx)=>
+                       <span key={idx}>
+                       <span>{items} </span>
+                       </span>)}
+                    <p>Language: {item.show.language}</p>
+                    <p>Status: {item.show.status}</p>
+                    {item.show.summary ? <p id="summary">Summary: {item.show.summary.replace('<p>','').replace('</p>','').replace('<b>','').replace('</b>','').replace('<i>','').replace('</i>','')}</p> : null} 
+                    <p>Ratings: {item.show.rating.average}</p>
+                </div>
+
             </div>
         ))
 
