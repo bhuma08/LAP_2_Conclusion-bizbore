b75ac7a1affdf547b6e246ba0526b5aed1b591d2
bhuma08 <62709043+bhuma08@users.noreply.github.com>
2020-09-25 19:51:50 +0100

    rid of codes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <Nav/> */}
         
         <Switch>
         <Route exact path = "/" component ={Home}></Route>
